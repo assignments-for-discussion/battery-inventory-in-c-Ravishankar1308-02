a778b435ac0f2b2d74aab5419981335b035ed9d2
Ravishankar1308-02 <93141028+Ravishankar1308-02@users.noreply.github.com>
2023-09-08 20:12:41 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -9,11 +9,27 @@ struct CountsBySoH {
 
 struct CountsBySoH countBatteriesByHealth(const int* presentCapacities, int nBatteries) {
   struct CountsBySoH counts = {0, 0, 0};
+    const int ratedCapacity = 120;  // Rated capacity of a new battery
+
+  for (int i = 0; i < nBatteries; i++) {
+    // Calculate SoH for each battery
+    double soh = (double)presentCapacities[i] / ratedCapacity * 100;
+
+    // Classify the battery based on SoH
+    if (soh > 80) {
+      counts.healthy++;
+    } else if (soh >= 65) {
+      counts.exchange++;
+    } else {
+      counts.failed++;
+    }
+  }
+
   return counts;
 }
 
 void testBucketingByHealth() {
-  const int presentCapacities[] = {115, 118, 80, 95, 91, 77};
+  const int presentCapacities[] = {115, 118, 80, 95, 91, 55};
   const int numberOfBatteries = sizeof(presentCapacities) / sizeof(presentCapacities[0]);
   printf("Counting batteries by SoH...\n");
   struct CountsBySoH counts = countBatteriesByHealth(presentCapacities, numberOfBatteries);
@@ -21,6 +37,9 @@ void testBucketingByHealth() {
   assert(counts.exchange == 3);
   assert(counts.failed == 1);
   printf("Done counting :)\n");
+  printf("healthy is: %d\n",counts.healthy);
+  printf("exchange is: %d\n",counts.exchange);
+  printf("failed is: %d\n",counts.failed);
 }
 
 int main() {
